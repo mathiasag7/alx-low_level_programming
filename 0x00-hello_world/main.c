2fa03b34b7f1f2a9de90f172de220dc1bb7664e5
mathiasag7 <mathiasag07@gmail.com>
2022-03-19 15:46:01 -0700

    write c file output into another file

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,6 +1,3 @@
 #include <stdio.h>
-
-/** * main - Entry point * * Return: Always 0 (Success) */ 
-int main(void) { 
-    return (0);
-}
+/** * main - Entry point * * Return: Always 0 (Success) */
+int main(void) {return (0); }
