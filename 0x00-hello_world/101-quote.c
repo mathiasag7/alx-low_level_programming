91ff16b02baa3a31073453f4311d8c1c310b4e1a
mathiasag7 <mathiasag07@gmail.com>
2022-07-07 07:11:04 -0700

    e

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include<unistd.h>
 
 /**
@@ -8,6 +7,6 @@
 
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
-return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
+ return (1);
 }
