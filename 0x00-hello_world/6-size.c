84f746803fb2647b2a6c885e2cdf510b2bf27e63
mathiasag7 <mathiasag07@gmail.com>
2022-07-07 06:31:51 -0700

    e

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %zu bytes\n", (unsigned long)sizeof(a));
-printf("Size of int: %zu bytes\n", (unsigned long)sizeof(b));
-printf("Size of long int: %zu bytes\n", (unsigned long)sizeof(c));
-printf("Size of long long int: %zu byte\n", (unsigned long)sizeof(d));
-printf("Size of float: %zu byte\n", (unsigned long)sizeof(e));
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of long int: %zu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of long long int: %zu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof(e));
 
 return (0);
 }
